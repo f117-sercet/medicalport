fe0e58216f0f74eadb0e6da3f2a18c6b677bca35
60221 <15159885771@163.com>
2021-10-06 17:05:49 +0800

    hospital-serviceImpl

--- yygh_parent/hospital-manage/src/main/java/com/hosp/service/impl/hospitalServiceImpl.java
+++ yygh_parent/hospital-manage/src/main/java/com/hosp/service/impl/hospitalServiceImpl.java
@@ -3,6 +3,8 @@ package com.hosp.service.impl;
 import com.alibaba.fastjson.JSONObject;
 import com.hosp.mapper.OrderInfoMapper;
 import com.hosp.mapper.ScheduleMapper;
+import com.hosp.model.OrderInfo;
+import com.hosp.model.Patient;
 import com.hosp.model.Schedule;
 import com.hosp.service.HospitalService;
 import com.hosp.utils.HospitalException;
@@ -12,6 +14,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -35,22 +39,68 @@ public class hospitalServiceImpl implements HospitalService {
 
         log.info(JSONObject.toJSONString(paramMap));
         String hoscode = (String) paramMap.get("hoscode");
-        String depcode = (String)paramMap.get("depcode");
-        String hosScheduleId = (String)paramMap.get("hosScheduleId");
-        String reserveDate = (String)paramMap.get("reserveDate");
-        String reserveTime = (String)paramMap.get("reserveTime");
-        String amount = (String)paramMap.get("amount");
+        String depcode = (String) paramMap.get("depcode");
+        String hosScheduleId = (String) paramMap.get("hosScheduleId");
+        String reserveDate = (String) paramMap.get("reserveDate");
+        String reserveTime = (String) paramMap.get("reserveTime");
+        String amount = (String) paramMap.get("amount");
 
         Schedule schedule = this.getSchedule("1L");
-        if (null == schedule){
+        if (null == schedule) {
 
-            throw  new HospitalException(ResultCodeEnum.DATA_ERROR);
+            throw new HospitalException(ResultCodeEnum.DATA_ERROR);
+    }
+        if (!schedule.getHoscode().equals(hoscode)||
+        !schedule.getDepcode().equals(depcode)||
+                schedule.getAmount().equals(amount)){
+
+            throw  new HospitalException(ResultCodeEnum.DATA_ERROR)
         }
 
+        //就诊人信息
+        Patient patient = JSONObject.parseObject(JSONObject.toJSONString(paramMap),Patient.class)
+        log.info(JSONObject.toJSONString(patient));
 
+        //处理就诊业务
+        Long patientId = this.savePatient(patient);
+        Map<String, Object> resultMap = new HashMap<>();
+        int availableNumber = schedule.getAvailableNumber().intValue() - 1;
+        if(availableNumber > 0) {
+            schedule.setAvailableNumber(availableNumber);
+            hospitalMapper.updateById(schedule);
 
+            //记录预约记录
+            OrderInfo orderInfo = new OrderInfo();
+            orderInfo.setPatientId(patientId);
+            orderInfo.setScheduleId(1L);
+            int number = schedule.getReservedNumber().intValue() - schedule.getAvailableNumber().intValue();
+            orderInfo.setNumber(number);
+            orderInfo.setAmount(new BigDecimal(amount));
+            String fetchTime = "0".equals(reserveDate) ? " 09:30前" : " 14:00前";
+            orderInfo.setFetchTime(reserveTime + fetchTime);
+            orderInfo.setFetchAddress("一楼9号窗口");
+            //默认 未支付
+            orderInfo.setOrderStatus(0);
+            orderInfoMapper.insert(orderInfo);
 
-        return null;
+            resultMap.put("resultCode","0000");
+            resultMap.put("resultMsg","预约成功");
+            //预约记录唯一标识（医院预约记录主键）
+            resultMap.put("hosRecordId", orderInfo.getId());
+            //预约号序
+            resultMap.put("number", number);
+            //取号时间
+            resultMap.put("fetchTime", reserveDate + "09:00前");;
+            //取号地址
+            resultMap.put("fetchAddress", "一层114窗口");;
+            //排班可预约数
+            resultMap.put("reservedNumber", schedule.getReservedNumber());
+            //排班剩余预约数
+            resultMap.put("availableNumber", schedule.getAvailableNumber());
+        } else {
+            throw new HospitalException(ResultCodeEnum.DATA_ERROR);
+        }
+        return resultMap;
     }
 
     @Override
