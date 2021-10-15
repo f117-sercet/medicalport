468a0b4d865264f5dac7d51b02ed6c2eddb61d3b
60221 <15159885771@163.com>
2021-10-15 22:27:01 +0800

    HospitalController

--- yygh_parent/hospital-manage/src/main/java/com/hosp/controller/HospitalController.java
+++ yygh_parent/hospital-manage/src/main/java/com/hosp/controller/HospitalController.java
@@ -1,8 +1,52 @@
 package com.hosp.controller;
 
+import com.hosp.service.ApiService;
+import com.hosp.service.HospitalService;
+import com.hosp.utils.HospitalException;
+import com.hosp.utils.HttpRequestHelper;
+import com.hosp.utils.Result;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
 /**
  * @author dc
  * 医院
  */
+@Api(tags = "医院管理接口")
+@RestController
 public class HospitalController {
+
+    @Autowired
+    private HospitalService hospitalService;
+
+    @Autowired
+    private ApiService apiService;
+
+    /**
+     * 预约下单
+     *
+     * @param request
+     * @return
+     */
+    @PostMapping("/order/submitOrder")
+    public Result AgreeAccountLendProject(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            Map<String, Object> paramMap = HttpRequestHelper.switchMap(request.getParameterMap());
+//			if(!HttpRequestHelper.isSignEquals(paramMap, apiService.getSignKey())) {
+//				throw new YyghException(ResultCodeEnum.SIGN_ERROR);
+//			}
+
+            Map<String, Object> resultMap = hospitalService.submitOrder(paramMap);
+            return Result.ok(resultMap);
+        } catch (HospitalException e) {
+            return Result.fail().message(e.getMessage());
+        }
+    }
+
 }
