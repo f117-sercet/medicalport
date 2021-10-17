021d6ea04d8be269d3c00e6ba9d65d8eb6bd10b6
60221 <15159885771@163.com>
2021-10-17 22:39:38 +0800

    config

---
+++ yygh_parent/hospital-manage/src/main/java/com/hosp/config/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.hosp.config;
+
+import com.hosp.utils.HospitalException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+/**
+ * 全局异常处理
+ * @author  info
+ */
+@ControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public String error(Exception e){
+        e.printStackTrace();
+        return "error";
+
+    }
+    /**
+     * 自定义异常处理方法
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(HospitalException.class)
+    public String error(HospitalException e, Model model){
+        model.addAttribute("message", e.getMessage());
+        return "error";
+    }
+}
