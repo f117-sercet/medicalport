c6f62512983ad4723a415e918b056f8ed900ac23
60221 <15159885771@163.com>
2021-10-18 10:11:20 +0800

    配置文件完成

---
+++ yygh_parent/hospital-manage/src/main/java/com/hosp/HospitalApplication.java
@@ -0,0 +1,20 @@
+package com.hosp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.activation.DataSource;
+
+/**
+ * 启动类
+ * @author dsc
+ */
+@SpringBootApplication
+public class HospitalApplication {
+
+    public static void main(String[] args) {
+
+            SpringApplication.run(HospitalApplication.class, args);
+        }
+
+    }
