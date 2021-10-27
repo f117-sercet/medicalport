cfe3303879581298e173f0f4c714895d725ed347
60221 <15159885771@163.com>
2021-10-27 16:50:31 +0800

    service-cmn

---
+++ yygh_parent/service/service_cnm/src/main/java/com/dsc/hosp/ServiceCnmApplication.java
@@ -0,0 +1,17 @@
+package com.dsc.hosp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+/**
+ * @author 60221
+ * 启动类
+ */
+@SpringBootApplication
+public class ServiceCnmApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceCnmApplication.class);
+    }
+}
