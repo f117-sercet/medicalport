590a1e72ac4fc5f95cf5a3e2df55f56183b444d3
60221 <15159885771@163.com>
2021-11-04 21:59:11 +0800

    数据字典

--- yygh_parent/service/service_cnm/src/main/java/com/dsc/hosp/service/impl/DictServiceImpl.java
+++ yygh_parent/service/service_cnm/src/main/java/com/dsc/hosp/service/impl/DictServiceImpl.java
@@ -1,15 +1,20 @@
 package com.dsc.hosp.service.impl;
 
+import com.alibaba.excel.EasyExcel;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.dsc.hosp.mapper.DictMapper;
 import com.dsc.hosp.service.DictService;
 import com.dsc.hospital.model.cmn.Dict;
+import com.dsc.hospital.vo.cmn.DictEeVo;
+import org.springframework.beans.BeanUtils;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -54,9 +59,36 @@ public class DictServiceImpl extends ServiceImpl<DictMapper,Dict> implements Dic
         return count>0;
     }
 
+    /**
+     * 导出数据字典
+     * @param response
+     */
     @Override
     public void exportDictData(HttpServletResponse response) {
 
+        //设置下载信息
+        response.setContentType("application/vnd，ms-excel");
+        response.setCharacterEncoding("utf-8");
+        String fileName="dict";
+        response.setHeader("Content-disposition", "attachment;filename="+ fileName + ".xlsx");
+
+        //查询数据库
+        List<Dict> dictList = baseMapper.selectList(null);
+        //Dict -- DictEeVo
+        List<DictEeVo> dictVoList = new ArrayList<>();
+        for(Dict dict:dictList) {
+            DictEeVo dictEeVo = new DictEeVo();
+            // dictEeVo.setId(dict.getId());
+            BeanUtils.copyProperties(dict,dictEeVo);
+            dictVoList.add(dictEeVo);
+        }
+        //调用方法进行写操作
+        try {
+            EasyExcel.write(response.getOutputStream(), DictEeVo.class).sheet("dict")
+                    .doWrite(dictVoList);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
