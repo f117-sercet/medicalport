5213d9561fe5d8daff0988f8281f364c538456a2
60221 <15159885771@163.com>
2021-11-09 18:05:37 +0800

    分页插件

---
+++ yygh_parent/service/service_cnm/src/main/java/com/dsc/hosp/config/CmnConfig.java
@@ -0,0 +1,23 @@
+package com.dsc.hosp.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author dsc
+ * 配置文件
+ */
+@Configuration
+@MapperScan("com.dsc.hosp.mapper")
+public class CmnConfig {
+    /**
+     * 分页插件
+     * @return
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+}
