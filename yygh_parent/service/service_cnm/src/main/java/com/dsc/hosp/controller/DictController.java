0f0f8711625d36271a5a4cf14c0cd10ccdc6ffc4
60221 <15159885771@163.com>
2021-11-10 15:15:05 +0800

    Redis配置与设置

---
+++ yygh_parent/service/service_cnm/src/main/java/com/dsc/hosp/controller/DictController.java
@@ -0,0 +1,23 @@
+package com.dsc.hosp.controller;
+
+import com.dsc.hosp.service.DictService;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author dsc
+ * 数据字典控制器
+ */
+@Api(description = "数据字典接口")
+@RestController
+@RequestMapping("/admin/cmn/dict")
+//@CrossOrigin
+public class DictController {
+
+    @Autowired
+    private DictService dictService;
+
+}
