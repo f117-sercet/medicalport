5976273b3b3e67adc4e4fd68195c267ed671f291
60221 <15159885771@163.com>
2021-11-15 17:27:34 +0800

    导入导出数据字典

--- yygh_parent/service/service_cnm/src/main/java/com/dsc/hosp/controller/DictController.java
+++ yygh_parent/service/service_cnm/src/main/java/com/dsc/hosp/controller/DictController.java
@@ -1,11 +1,16 @@
 package com.dsc.hosp.controller;
 
+import com.dsc.hosp.result.Result;
 import com.dsc.hosp.service.DictService;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * @author dsc
@@ -19,5 +24,19 @@ public class DictController {
 
     @Autowired
     private DictService dictService;
+
+    //导入数据字典
+    @PostMapping("importData")
+    public Result importDictDataa(MultipartFile file){
+        dictService.importDictData(file);
+        return Result.ok();
+    }
+    //导出数据字典接口
+    @GetMapping("exportData")
+    public void exportDict(HttpServletResponse response) {
+        dictService.exportDictData(response);
+    }
+
+
 
 }
