c2962f700aa43981de630942b6c688632ac585f6
60221 <15159885771@163.com>
2021-11-15 17:48:50 +0800

    controller中其他方法

--- yygh_parent/service/service_cnm/src/main/java/com/dsc/hosp/controller/DictController.java
+++ yygh_parent/service/service_cnm/src/main/java/com/dsc/hosp/controller/DictController.java
@@ -2,15 +2,15 @@ package com.dsc.hosp.controller;
 
 import com.dsc.hosp.result.Result;
 import com.dsc.hosp.service.DictService;
+import com.dsc.hospital.model.cmn.Dict;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 /**
  * @author dsc
@@ -38,5 +38,34 @@ public class DictController {
     }
 
 
+    //根据dictCode获取下级节点
+    @ApiOperation(value = "根据dictCode获取下级节点")
+    @GetMapping("findByDictCode/{dictCode}")
+    public Result findByDictCode(@PathVariable String dictCode) {
+        List<Dict> list = dictService.findByDictCode(dictCode);
+        return Result.ok(list);
+    }
 
+    //根据数据id查询子数据列表
+    @ApiOperation(value = "根据数据id查询子数据列表")
+    @GetMapping("findChildData/{id}")
+    public Result findChildData(@PathVariable Long id) {
+        List<Dict> list = dictService.findChildData(id);
+        return Result.ok(list);
+    }
+
+    //根据dictcode和value查询
+    @GetMapping("getName/{dictCode}/{value}")
+    public String getName(@PathVariable String dictCode,
+                          @PathVariable String value) {
+        String dictName = dictService.getDictName(dictCode,value);
+        return dictName;
+    }
+
+    //根据value查询
+    @GetMapping("getName/{value}")
+    public String getName(@PathVariable String value) {
+        String dictName = dictService.getDictName("",value);
+        return dictName;
+    }
 }
